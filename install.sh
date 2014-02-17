42e0a3dcbfd152c319d386c19c4138c0c17289ee
Henry Baba-Weiss <henry.babaweiss@gmail.com>
2014-02-16 23:59:10 -0800

    Updated install script

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-sudo cp capsremap.map /usr/share/keymaps/capsremap.map
-find . -maxdepth 1 -name '.*' ! -name '.git' | xargs -I file cp -r file ~
+find . -maxdepth 1 -name '*' ! -name '.git' ! -name 'install.sh' | xargs -I file cp -r file ~
 
