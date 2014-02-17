e18f7807f5af27987f666556bfbe4bf7dc85c8fa
Henry Baba-Weiss <henry.babaweiss@gmail.com>
2014-02-16 23:39:26 -0800

    fixed install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo cp capsremap.map /usr/share/keymaps
-find . -maxdepth 1 -name '.*' ! -name '.git' | xargs cp ~
+find . -maxdepth 1 -name '.*' ! -name '.git' | xargs -I file cp -r file ~
 
