52d922560327daabd4551a21f9d4a6e4863634b4
Henry Baba-Weiss <henry.babaweiss@gmail.com>
2014-02-16 23:53:09 -0800

    Fix bug in install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo cp capsremap.map /usr/share/keymaps
+sudo cp capsremap.map /usr/share/keymaps/capsremap.map
 find . -maxdepth 1 -name '.*' ! -name '.git' | xargs -I file cp -r file ~
 
