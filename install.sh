cacff1b60b428c1b4197735a0c19db850cf33d0e
Henry Baba-Weiss <henry.babaweiss@gmail.com>
2014-02-17 00:02:42 -0800

    Fix more bugs

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find . -maxdepth 1 -name '*' ! -name '.git' ! -name 'install.sh' | xargs -I file cp -r file ~
+find . -maxdepth 1 -name '*' ! -name '.git' ! -name 'install.sh' ! -name '.' | xargs -I file cp -r file ~/file
 
