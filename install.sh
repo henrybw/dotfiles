743720d005fa702704951c89691751537478a6e2
Henry Baba-Weiss <henry.babaweiss@gmail.com>
2014-02-17 00:12:56 -0800

    Added bin

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-find . -maxdepth 1 -name '*' ! -name '.git' ! -name 'install.sh' ! -name '.' | xargs -I file cp -r file ~/file
+find . -maxdepth 1 -name '*' ! -name '.git' ! -name 'install.sh' ! -name 'bin' ! -name '.' | xargs -I file cp -r file ~/file
+
+if [ ! -e ~/bin ]; then
+    mkdir ~/bin;
+fi
+
+cp bin/* ~/bin
 
