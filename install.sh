4ff92c3ec5e86ec2b7f247164bf4e74a85beb36b
Henry Baba-Weiss <henry.babaweiss@gmail.com>
2014-08-03 18:32:25 -0700

    Fix typos and bugs in install script

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 find . -maxdepth 1 -name '*' ! -name '.git' ! -name 'install.sh' ! -name 'bin' ! -name '.' | xargs -I file cp -r file ~/file
 
 if [ ! -e /usr/share/keymaps ]; then
-    sudo mkdr /usr/share/keymaps;
+    sudo mkdir /usr/share/keymaps;
 fi
-cp ./capsremap.map /usr/share/keymaps
+sudo cp ./capsremap.map /usr/share/keymaps
 
 if [ ! -e ~/bin ]; then
     mkdir ~/bin;
