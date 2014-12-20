229a339774fd3c3e620873225811d1f1970e7709
henrybw <henry.babaweiss@gmail.com>
2014-12-20 01:48:46 -0800

    Update persistscreen script and fix /home/hbw references

--- bin/dyndns.sh
+++ bin/dyndns.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 dig +short myip.opendns.com @resolver1.opendns.com > /tmp/ip.txt
-ssh-add /home/hbw/.ssh/id_persistscreen
+eval $(ssh-agent)
+ssh-add $HOME/.ssh/id_persistscreen
 scp /tmp/ip.txt henryb@persistscreen.com:~/ip.txt
